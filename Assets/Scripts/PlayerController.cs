feac456001a22665c94c30083222927ee1cc2984
jajuang <josephine.a.juang@intel.com>
2015-08-22 02:46:37 -0700

    Updated git settings for Unity Project

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class testcontroller : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
